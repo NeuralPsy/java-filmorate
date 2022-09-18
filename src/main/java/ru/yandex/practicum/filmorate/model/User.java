97b112eb5d497450390e08af1b3eba84efe89113
NeuralPsy <kirill.neuralpsy@gmail.com>
2022-09-19 00:52:00 +0600

    Add Film and User classes in model package

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jdk.jfr.DataAmount;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+
+    int id;
+    String email;
+    String login;
+    String name;
+    LocalDate birthday;
+
+
+
+}
